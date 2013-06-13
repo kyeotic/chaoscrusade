61fce66da019bde11de945803f44c5629c4f11dc
tyrsius <tyrsius@gmail.com>
2013-06-12 21:45:51 -0700

    fix socket module

--- app_modules/sockets.js
+++ app_modules/sockets.js
@@ -1,16 +1,22 @@
 
 module.exports = function(io) {
 	
-	this.middleware = function(req, res, next) {
+	var middleware = function(req, res, next) {
 		req.socketId = req.headers['x-socket-id'];
 		next();
 	};
 
-	this.broadcast = function(socketId, eventName, message) {
+	var broadcast = function(socketId, eventName, message) {
 		if (!socketId || !eventName || !message)
 			return;
 		io.sockets[socketId].broadcast.emit(eventName, message);
 	};
 
-	this.on = io.sockets.on;
+	var on = io.sockets.on;
+
+	return {
+		middleware: middleware,
+		broadcast: broadcast,
+		on: on
+	};
 };
