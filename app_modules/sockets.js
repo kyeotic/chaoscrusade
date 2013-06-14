15f586f5923e5e9065aa452fc4cbe23b43f9f9a8
Timothy Moran <tyrsius@gmail.com>
2013-06-14 08:43:24 -0700

    add socket broadcast to all when socketId missing

--- app_modules/sockets.js
+++ app_modules/sockets.js
@@ -7,9 +7,15 @@ module.exports = function(io) {
 	};
 
 	var broadcast = function(socketId, eventName, message) {
-		if (!socketId || !eventName || !message)
+		if (!eventName || !message)
 			return;
-		io.sockets[socketId].broadcast.emit(eventName, message);
+		
+		//If we have no socket, broadcast to everyone
+		//Because the connection came from a non-websocket source
+		if (!socketId)
+			io.sockets.emit(eventName, message);
+		else
+			io.sockets[socketId].broadcast.emit(eventName, message);
 	};
 
 	var on = io.sockets.on;
