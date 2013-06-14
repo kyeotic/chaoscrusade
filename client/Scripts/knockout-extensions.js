0529683edfa0daee29a289d391db8c9129ae1bd3
Tyrsius <tyrsius@gmail.com>
2013-06-13 20:52:09 -0700

    adding observableArray.subscribeArrayChanged

--- client/Scripts/knockout-extensions.js
+++ client/Scripts/knockout-extensions.js
@@ -15,6 +15,31 @@ ko.observableArray.fn.pushAll = function(items){
     return this.peek().length;
 };
 
+ko.observableArray.fn.subscribeArrayChanged = function(addCallback, deleteCallback) {
+    var previousValue = undefined;
+    this.subscribe(function(_previousValue) {
+        previousValue = _previousValue.slice(0);
+    }, undefined, 'beforeChange');
+    this.subscribe(function(latestValue) {
+        var editScript = ko.utils.compareArrays(previousValue, latestValue);
+        for (var i = 0, j = editScript.length; i < j; i++) {
+            switch (editScript[i].status) {
+                case "retained":
+                    break;
+                case "deleted":
+                    if (deleteCallback)
+                        deleteCallback(editScript[i].value);
+                    break;
+                case "added":
+                    if (addCallback)
+                        addCallback(editScript[i].value);
+                    break;
+            }
+        }
+        previousValue = undefined;
+    });
+};
+
 ko.extenders.numeric = function(target, options) {
     //create a writeable computed observable to intercept writes to our observable
     var result = ko.computed({
